4d5a4b26ca5faebc70cec74870fd80add58a4d19
Dan <danporeda@yahoo.com>
2023-12-06 20:32:46 -0500

    Promise, .then, .catch

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-const willGetYouADog = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const rand = Math.random();
-        console.log(rand);
-        if (rand < 0.5) {
-            resolve();
-        }
-        else {
-            reject();
-        }
-    }, 5000)
-});
-willGetYouADog.then(() => {
-    console.log('We got a dog!');
-});
-willGetYouADog.catch(() => {
-    console.log('No Dog');
-});
+// const makeDogPromise = () => {
+// return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const rand = Math.random();
+//         console.log(rand);
+//         if (rand < 0.5) {
+//             resolve();
+//         }
+//         else {
+//             reject();
+//         }
+//     }, 5000)
+// });
+// };
+// makeDogPromise()
+//     .then(() => {
+//         console.log('We got a dog!');
+//     })
+//     .catch(() => {
+//         console.log('No Dog');
+// });
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const rand = Math.random();
+            if (rand < 0.3) {
+                reject({status : 404});
+            }
+            else {
+                const pages = {
+                    '/users': [
+                        {id: 1, username: 'Bilbo'},
+                        {id: 5, username: 'Esmerelda'}
+                    ],
+                    '/about' : 'This is the about page!'
+                };
+                const data = pages[url];
+                resolve({ status: 200, data});
+            }
+        }, 3000);
+    });
+};
+
+fakeRequest('/about')
+    .then((res) => {
+        console.log('Status Code', res.status);
+        console.log('Data:', res.data);
+        console.log('Request Worked');
+        })
+    .catch((res) => {
+        console.log(res.status);
+        console.log('Request Failed');
+    });
